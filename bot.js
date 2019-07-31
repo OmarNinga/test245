31a261726968638b2130790e6f98e85a42db1269
OmarNinga <47434394+OmarNinga@users.noreply.github.com>
2019-07-31 07:44:36 +0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("605922280589099014")
+setInterval(function() {
+channel.send(`omar omar omaz omar omar`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
